6aa6bb38dd671c9f34f62ede73dae22d98a31132
raylinw <raylin.wang@quantatw.com>
2016-08-26 18:33:52 +0800

    increase version to 0.0.5

--- include/version.h
+++ include/version.h
@@ -27,7 +27,7 @@
 #include "../uboot_version.h"
 #include <configs/rt2880.h>
 
-#define U_BOOT_VERSION	"0.0.4"
+#define U_BOOT_VERSION	"0.0.5"
 
 #if defined (RT2880_ASIC_BOARD) || defined (RT2883_ASIC_BOARD) || defined (RT3052_ASIC_BOARD) || defined (RT3352_ASIC_BOARD) || defined (RT3883_ASIC_BOARD) || defined (RT5350_ASIC_BOARD) || defined (RT6855_ASIC_BOARD) || defined (RT6855A_ASIC_BOARD) || defined (MT7620_ASIC_BOARD) || defined (MT7621_ASIC_BOARD) ||defined (MT7628_ASIC_BOARD)
 #define CHIP_TYPE	"ASIC"
